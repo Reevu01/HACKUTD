28c1565ee83d42af908096221376083f924f7cc8
Reevu01 <reevu1214@gmail.com>
2024-10-19 00:05:31 -0500

    repo

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./templates/**/*.html", // Adjust this if your templates are in different directories
+    "./static/**/*.js", // Include any JavaScript files that might use Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
